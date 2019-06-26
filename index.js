d45915ae9ecb58283b0c5de4f47a2fe2c249549c
Bkreps <krepsbryan@gmail.com>
2019-06-26 02:50:51 -0400

    Added animate.css. Animated banner and banner txt

---
+++ index.js
@@ -0,0 +1,11 @@
+// $(function() {
+//     $('#enlarge').animate({ height: '100%', width: '100%' }, 1000, 'linear');
+//   });
+
+// $( document ).click(function() {
+//   $( "#enlarge" ).ready({ effect: "scale", direction: "horizontal", origin: "center"}).removeclass('hidden');
+// });
+
+// $(document).ready(function () {
+//   $('div.hidden').fadeIn(1000).removeClass('hidden');
+// });
