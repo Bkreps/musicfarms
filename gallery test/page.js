f2bf6de1fc4c5123bc57bb5c944f0f21cab01fe2
Bkreps <krepsbryan@gmail.com>
2019-07-08 02:10:26 -0400

    edits to test gallery

--- gallery test/page.js
+++ gallery test/page.js
@@ -1,21 +1,40 @@
+document.querySelectorAll('.imggal').addEventListener('click', function() {
+  // openModal();
+});
+
+
+
+document.querySelector('.close').addEventListener('click', function() {
+  document.getElementById("myModal").style.display = "none";
+});
+
+document.querySelector('.prev').addEventListener('click', function() {
+    plusSlides(-1);
+  });
+ document.querySelector('.next').addEventListener('click', function() {
+    plusSlides(1);
+  });
+  
+
+ 
 function openModal() {
-    document.getElementById("myModal").style.display = "block";
-  }
-  
-  function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-  }
-  
-//   var slideIndex = 1;
-//   showSlides(slideIndex);
-  
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-  function currentSlide(n) {
+  document.getElementById("myModal").style.display = "block";
+}
+
+
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
     showSlides(slideIndex = n);
   }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
   
   function showSlides(n) {
     var slides = document.getElementsByClassName("slides");
@@ -30,3 +49,19 @@ function openModal() {
     }
     slides[slideIndex-1].style.display = "block";
   }
+
+
+  //       <div class="row">
+  //               <div class="column">
+  //                 <img src="/gallery test/vatraandcosmic/vandc1.jpg" style="width:100%" onclick="openModal();currentSlide(1)" class="hover-shadow cursor">
+  //               </div>
+  //               <div class="column">
+  //                 <img src="/gallery test/vatraandcosmic/vandc2.jpg" style="width:100%" onclick="openModal();currentSlide(2)" class="hover-shadow cursor">
+  //               </div>
+  //               <div class="column">
+  //                 <img src="/gallery test/vatraandcosmic/vandc3.jpg" style="width:100%" onclick="openModal();currentSlide(3)" class="hover-shadow cursor">
+  //               </div>
+  //               <div class="column">
+  //                 <img src="/gallery test/vatraandcosmic/vandc4.jpg" style="width:100%" onclick="openModal();currentSlide(4)" class="hover-shadow cursor">
+  //               </div>
+  //             </div>
